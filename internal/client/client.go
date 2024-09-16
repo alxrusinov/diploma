b99647c56320144b7ab2492067443f12a3fd3233
Aleksei Rusinov <alxrusinov@yandex.ru>
2024-09-16 18:08:56 +0200

    request to accrual service

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,6 +1,10 @@
 package client
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/alxrusinov/diploma/internal/model"
@@ -10,8 +14,31 @@ type Client struct {
 	client *http.Client
 }
 
-func (client *Client) GetOrderInfo(order string) (*model.Order, error) {
-	return nil, nil
+func (client *Client) GetOrderInfo(orderNumber string) (*model.Order, error) {
+
+	addr := fmt.Sprintf("/api/orders/%s", orderNumber)
+
+	req, err := http.NewRequest(http.MethodGet, addr, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := client.client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	order := &model.Order{}
+
+	if err := json.NewDecoder(res.Request.Body).Decode(&order); err != nil && !errors.Is(err, io.EOF) {
+		return nil, err
+	}
+
+	return order, nil
 }
 
 func CreateClient() *Client {
